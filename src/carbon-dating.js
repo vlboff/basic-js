8f8605d29c211be0425fc5a79fd071f6fbeb1af1
Uladzimir <vl.barbikov@gmail.com>
2022-10-16 20:35:47 +0300

    feat: add carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity == 'string') {
+  let age = Number(sampleActivity);
+    if (age > 0 && age < MODERN_ACTIVITY) {
+      let k = 0.693 / HALF_LIFE_PERIOD;
+      let year = Math.log(MODERN_ACTIVITY / age) / k;
+      return Math.ceil(year);
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
